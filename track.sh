4484ee9efaf0b4913e0b58231f5c53c7b327a986
Beichen Wang <beichenwang2000@gmail.com>
2025-09-06 23:10:47 -0400

    Update track.sh

--- track.sh
+++ track.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# 设置索引范围，例如从1到10
 start_index=1
 end_index=9
 
-# 遍历指定的索引范围
 for i in $(seq $start_index $end_index); do
-    # 设置输入视频路径
     input_video="/home/bw2716/SeeUnsafe/data/video_${i}.mp4"
-    # 设置输出视频路径
     output_video="/home/bw2716/SeeUnsafe/data/video_${i}_track.mp4"
     
-    # 运行 Python 脚本
     python track_objects.py --input "$input_video" --output "$output_video" --num_key_frames 9 --bbx_file /home/bw2716/SeeUnsafe/bbx_file.csv --index_file /home/bw2716/SeeUnsafe/index_file.csv
 done
